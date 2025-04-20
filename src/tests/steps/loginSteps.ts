da4c57b5e22cdd78ea397e64ed2f70d448a3f37b
sollmayer <stupanenko.serhii@gmail.com>
2025-04-20 19:18:28 +0300

    refactor to use pageFixture

--- src/tests/steps/loginSteps.ts
+++ src/tests/steps/loginSteps.ts
@@ -1,26 +1,20 @@
 import {Given, When, Then} from '@cucumber/cucumber';
 import { chromium, Browser, Page, expect } from '@playwright/test';
-
-let browser: Browser;
-let page: Page;
+import { pageFixture } from '../../hooks/pageFixture';
 
 Given("the user is on the Login page", async function () {
-    browser = await chromium.launch({headless:false});
-    page = await browser.newPage();
-    
-    await page.goto('https://conduit.bondaracademy.com/');
-    await page.getByRole('link', {name:"Sign in"}).click();
+    await pageFixture.page.goto('https://conduit.bondaracademy.com/');
+    await pageFixture.page.getByRole('link', {name:"Sign in"}).click();
 })
 
 When("the user enters valid credentials", async function() {
-    await page.getByRole("textbox", { name: "Email" }).fill("TestingApiCalls@gmail.com");
-    await page.getByRole("textbox", { name: "Password" }).fill("TestingApiCalls");
+    await pageFixture.page.getByRole("textbox", { name: "Email" }).fill("TestingApiCalls@gmail.com");
+    await pageFixture.page.getByRole("textbox", { name: "Password" }).fill("TestingApiCalls");
 })
 When("the user clicks on login button", async function() {
-    await page.getByRole("button", { name: "Sign in" }).click();
+    await pageFixture.page.getByRole("button", { name: "Sign in" }).click();
 })
 Then('the user is redirected to the Home page', async function() {
-    await expect(page).toHaveURL('https://conduit.bondaracademy.com/')
-    await expect(page.getByRole('link', {name:"Sign in"})).not.toBeVisible();
-    await browser.close();
+    await expect(pageFixture.page).toHaveURL('https://conduit.bondaracademy.com/')
+    await expect(pageFixture.page.getByRole('link', {name:"Sign in"})).not.toBeVisible();
 })
